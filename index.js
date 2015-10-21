389966203b5535469e43b865b63926f39ba5d679
Ivan <ivan@eriad.com>
2015-10-22 02:40:53 +0800

    update some style of local script i was using

--- index.js
+++ index.js
@@ -1,99 +1,15 @@
+'use strict';
+
+
 var fs         = require('fs');
-var http       = require('http');
-var url        = require('url');
+var request    = require('request');
 var sqlite3    = require('sqlite3').verbose();
 var lineReader = require('line-reader');
 
+
 /**
- * Fetches oui file
+ * Some consts
  */
-var fetch = function(opts, next) {
-
-  var handle  = function handle(res) {
-
-    var outFile = fs.createWriteStream(opts.txt);
-
-    var data  = function (chunk) {
-      outFile.write(chunk);
-    };
-    var end   = function () {
-      outFile.end();
-      return next(null);
-    };
-    var close = function() {
-      outFile.end();
-      fs.unlink(txt, function () {
-        return next(new Error('premature eof'));
-      });
-    };
-
-    res.setEncoding('utf8');
-    res.on('data', data).on('end', end).on('close', close);
-  };
-
-  var error   = function error(err) {
-    return next(err, opts);
-  };
-
-  var options = url.parse(opts.url);
-  options.agent = false;
-
-  fs.unlink(opts.txt, function (err) {
-    if (err) return next(err);
-
-    http
-      .request(options, handle)
-      .on('error', error)
-      .end();
-  });
-
-};
-
-var parse = function (opts, next) {
-
-  var process = function(db) {
-
-    lineReader.eachLine(opts.txt, function(line) {
-      var oui, name;
-      line = line.trimLeft().trimRight();
-
-      if (line.length > 15) {
-	oui  = line.substr(0,8).split('-').join('');
-	line = line.substr(9).trimLeft();
-	if (line.substr(0,5) === '(hex)')
-	  name = line.substr(6).trimLeft();
-      }
-
-      if ( (!!oui)            &&
-	   (oui.length === 6) &&
-	   (!!name)           &&
-	   (name.length > 0)     ) {
-
-	 var insertSTMT = 'INSERT INTO oui(oui, name) VALUES($oui, $name)';
-	 var parameters = { $oui: oui, $name: name };
-	 db.run(insertSTMT, parameters, function (err) {
-	   if (err) console.warn(err);
-	 });
-      }
-    }).then(function () {
-      return next();
-    });
-
-  };
-
-  fs.unlink(opts.sql, function (err) {
-    if (err) return next(err);
-    var db = new sqlite3.Database(opts.sql);
-    opts.db = db;
-
-    var createSTMT = 'CREATE TABLE IF NOT EXISTS oui(oui TEXT PRIMARY KEY, name TEXT)';
-    db.run(createSTMT, function (err) {
-      if (err) return next(err);
-      return process(db);
-    });
-  });
-
-};
 
 var options = {
   url: 'http://standards-oui.ieee.org/oui.txt',
@@ -103,28 +19,88 @@ var options = {
 
 options.db = new sqlite3.Database(options.sql);
 
-exports.rebuild = function(next) {
-  fetch(options, function(err) {
+
+function parse(next) {
+
+  var process = function(db) {
+
+    lineReader.eachLine(options.txt, function(line) {
+
+      line = line.trimLeft().trimRight();
+      if (line.length < 15) { return; }
+
+
+      // Get OUI
+      var oui = line.substr(0,8).split('-').join('');
+      if ( (oui) && (oui.length !== 6) ) { return; }
+
+
+      // Get Name
+      var name;
+      line = line.substr(9).trimLeft();
+      if (line.substr(0,5) === '(hex)') { name = line.substr(6).trimLeft(); }
+      if (name === undefined)           { return; }
+      if (name.length < 1)              { return; }
+
+
+      var stmt = 'INSERT INTO oui(oui, name) VALUES($oui, $name)';
+      var parm = { $oui: oui, $name: name };
+      db.run(stmt, parm, function (err) {
+        if (err) {
+          console.warn(err);
+        }
+      });
+    }).then(function () {
+        return next();
+      });
+
+  };
+
+  fs.unlink(options.sql, function (err) {
     if (err) return next(err);
-    parse(options, function(err) {
-      if (err) return next(err);
-      return next(null);
+    var db = new sqlite3.Database(options.sql);
+
+    options.db = db;
+
+    var stmt = 'CREATE TABLE IF NOT EXISTS oui(oui TEXT PRIMARY KEY, name TEXT)';
+    db.run(stmt, function (err) {
+      if (err) { return next(err); }
+      return process(db);
     });
   });
+
+}
+
+
+exports.rebuild = function (next) {
+
+  try {
+    fs.unlinkSync(options.txt);
+  } catch (e) {
+    console.warn('unlinking problem', e);
+  }
+
+  request(options.url)
+    .on('error', console.error)
+    .on('end', function () {
+      parse(next);
+    })
+    .pipe(fs.createWriteStream(options.txt));
 };
 
 exports.lookup = function (oui, next) {
 
   var _oui = oui.split('-').join('').split(':').join('').toUpperCase();
-  if (_oui.length != 6)
+  if (_oui.length != 6) {
     return next(new Error('invalid OUI'));
+  }
 
   var db    = options.db;
   var query = 'SELECT * FROM oui WHERE oui=$oui LIMIT 1';
   var param = { $oui: _oui };
 
   db.get(query, param, function (err, row) {
-    next(err ? err : null, (!!row) ? row.name : null );
+    next(err ? err : null, (!!row) ? row.name : null);
   });
 
 };

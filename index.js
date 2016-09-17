2f8dfa71090d597dad45909f9040627639de6ceb
Anthony Hernandez <anthony.hernandez@clownphobia.com>
2016-09-16 23:25:06 -0500

    Update app to be an exported anonymous object
    Support passing full mac address
    Support colon, period, and dash notation notation
    Support passing config
    Use http://linuxnet.ca/ieee/oui/ instead of the slow and clunky
    Update test to reflect new options

--- index.js
+++ index.js
@@ -1,128 +1,153 @@
 'use strict';
 
 
-var fs         = require('fs');
-var request    = require('request');
-var sqlite3    = require('sqlite3').verbose();
+var fs = require('fs');
+var request = require('request');
+var sqlite3 = require('sqlite3').verbose();
 var lineReader = require('line-reader');
 
 
-/**
- * Some consts
- */
-var rebuilding = false;
-var options = {
-  url: 'http://standards-oui.ieee.org/oui.txt',
+var defaults = {
+  url: 'http://linuxnet.ca/ieee/oui.txt',
   sql: __dirname + '/oui.db',
   txt: __dirname + '/oui.txt'
 };
 
-options.db = new sqlite3.Database(options.sql);
+var val = function(key, config) {
+  return config[key] || defaults[key];
+};
 
 
-function parse(next) {
+var MacLookup = function(config) {
+  var config = config || {};
 
-  var process = function(db) {
+  this.rebuilding = false;
 
-    lineReader.eachLine(options.txt, function(line, last) {
+  this.options = {};
+  this.options.url = val('url', config);
+  this.options.sql = val('sql', config);
+  this.options.txt = val('txt', config);
+  this.options.db = new sqlite3.Database(this.options.sql);
 
-      line = line.trimLeft().trimRight();
-      if (line.length < 15) { return; }
+};
 
+MacLookup.prototype.rebuild = function(next) {
+  var ml = this;
+  var parse = function(next) {
 
-      // Get OUI
-      var oui = line.substr(0,8).split('-').join('');
-      if ( (oui) && (oui.length !== 6) ) { return; }
+    var process = function(db) {
 
+      lineReader.eachLine(ml.options.txt, function(line, last) {
 
-      // Get Name
-      var name;
-      line = line.substr(9).trimLeft();
-      if (line.substr(0,5) === '(hex)') { name = line.substr(6).trimLeft(); }
-      if (name === undefined)           { return; }
-      if (name.length < 1)              { return; }
+        line = line.trimLeft().trimRight();
+        if (line.length < 15) {
+          return;
+        }
 
+        // Get OUI
+        var oui = line.substr(0, 8).split('-').join('');
+        if ((oui) && (oui.length !== 6)) {
+          return;
+        }
 
-      var stmt = 'INSERT INTO oui(oui, name) VALUES($oui, $name)';
-      var parm = { $oui: oui, $name: name };
+        // Get Name
+        var name;
+        line = line.substr(9).trimLeft();
+        if (line.substr(0, 5) === '(hex)') {
+          name = line.substr(6).trimLeft();
+        }
+        if (name === undefined) {
+          return;
+        }
+        if (name.length < 1) {
+          return;
+        }
 
-      db.run(stmt, parm, function (err) {
-        if (err) { console.warn(err); }
+        var stmt = 'INSERT INTO oui(oui, name) VALUES($oui, $name)';
+        var parm = {
+          $oui: oui,
+          $name: name
+        };
+
+        db.run(stmt, parm, function(err) {
+          if (err) {
+            console.warn(err);
+            console.warn(stmt);
+            console.warn(parm);
+          }
+        });
+
+        if (last) {
+          ml.rebuilding = false;
+          return next();
+        }
+      })
+
+    };
+
+    fs.unlink(ml.options.sql, function(err) {
+      if (err) return next(err);
+      ml.options.db = new sqlite3.Database(ml.options.sql);
+
+      var stmt = 'CREATE TABLE IF NOT EXISTS oui(oui TEXT PRIMARY KEY, name TEXT)';
+      ml.options.db.run(stmt, function(err) {
+        if (err) {
+          return next(err);
+        }
+        return process(ml.options.db);
       });
-
-      if (last) {
-        rebuilding = false;
-        return next();
-      }
-    })
-
-  };
-
-  fs.unlink(options.sql, function (err) {
-    if (err) return next(err);
-    var db = new sqlite3.Database(options.sql);
-
-    options.db = db;
-
-    var stmt = 'CREATE TABLE IF NOT EXISTS oui(oui TEXT PRIMARY KEY, name TEXT)';
-    db.run(stmt, function (err) {
-      if (err) { return next(err); }
-      return process(db);
     });
-  });
 
-}
+  }
 
 
-exports.rebuild = function (next) {
-
-  if (rebuilding) {
+  if (this.rebuilding) {
     return next(new Error('Already Rebuilding'));
   }
-  rebuilding = true;
+  this.rebuilding = true;
 
   try {
-    fs.unlinkSync(options.txt);
+    fs.unlinkSync(this.options.txt);
   } catch (e) {
     console.warn('unlinking problem', e);
   }
-
-  request(options.url)
+  request(this.options.url)
     .on('error', console.error)
-    .on('end', function () {
+    .on('end', function() {
       parse(next);
     })
-    .pipe(fs.createWriteStream(options.txt));
+    .pipe(fs.createWriteStream(this.options.txt));
 };
 
-exports.lookup = function (oui, next) {
+MacLookup.prototype.lookup = function(oui, next) {
 
   if (!oui || oui.length < 6) {
-    return next(new Error('Invalid Request'),null);
+    return next(new Error('Invalid Request'), null);
   }
 
-  var _oui = oui.split(/[-.:]/).join('').toUpperCase().substring(0,6);
+  var _oui = oui.split(/[-.:]/).join('').toUpperCase().substring(0, 6);
   if (_oui.length != 6) {
     return next(new Error('invalid OUI'));
   }
 
-  var db    = options.db;
   var query = 'SELECT * FROM oui WHERE oui=$oui LIMIT 1';
-  var param = { $oui: _oui };
+  var param = {
+    $oui: _oui
+  };
 
-  db.get(query, param, function (err, row) {
+  this.options.db.get(query, param, function(err, row) {
     next(err ? err : null, (!!row) ? row.name : null);
   });
 
 };
 
-exports.each = function (each, next) {
-
-  var db    = options.db;
+MacLookup.prototype.each = function(each, next) {
   var query = 'SELECT * FROM oui';
 
-  db.each(query, function (err, row) {
+  this.options.db.each(query, function(err, row) {
     each(err, row);
   }, next);
 
 };
+
+module.exports = new MacLookup();

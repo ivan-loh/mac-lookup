622aacf59957177fa36c029549e29b7d5956a561
ivan <ivan@eriad.com>
2020-05-31 12:13:32 +0800

    new version to use csv loaded in memory

--- index.js
+++ index.js
@@ -1,122 +1,41 @@
 'use strict';
 
-
-var fs = require('fs');
-var request = require('request');
-var sqlite3 = require('sqlite3').verbose();
-var lineReader = require('line-reader');
-
-
-var defaults = {
-  url: 'http://linuxnet.ca/ieee/oui.txt',
-  sql: __dirname + '/oui.db',
-  txt: __dirname + '/oui.txt'
-};
-
-var val = function(key, config) {
-  return config[key] || defaults[key];
-};
+const fs        = require('fs');
+const path      = require('path');
+const csvParser = require('@fast-csv/parse');
+const defaults  = { csv: __dirname + '/oui.csv', };
 
 
-var MacLookup = function(config) {
-  var config = config || {};
+const MacLookup = function(config) {
 
-  this.rebuilding = false;
+  if (!config) { config = {} }
 
-  this.options = {};
-  this.options.url = val('url', config);
-  this.options.sql = val('sql', config);
-  this.options.txt = val('txt', config);
-  this.options.db = new sqlite3.Database(this.options.sql);
+  this.rebuilding  = false;
+  this.db          = {};
+  this.options     = {};
+  this.options.csv = config.csv || defaults.csv
 
 };
+
+MacLookup.prototype.load = function(done) {
+  fs
+      .createReadStream(path.resolve(this.options.csv))
+      .pipe(csvParser.parse({headers: true}))
+      .on('data', row => { this.db[row.Assignment] = row['Organization Name']; })
+      .on('end', ()   => { done(); });
+}
 
 MacLookup.prototype.rebuild = function(next) {
-  var ml = this;
-  var parse = function(next) {
-
-    var process = function(db) {
-
-      lineReader.eachLine(ml.options.txt, function(line, last) {
-
-        line = line.trimLeft().trimRight();
-        if (line.length < 15) {
-          return;
-        }
-
-        // Get OUI
-        var oui = line.substr(0, 8).split('-').join('');
-        if ((oui) && (oui.length !== 6)) {
-          return;
-        }
-
-        // Get Name
-        var name;
-        line = line.substr(9).trimLeft();
-        if (line.substr(0, 5) === '(hex)') {
-          name = line.substr(6).trimLeft();
-        }
-        if (name === undefined) {
-          return;
-        }
-        if (name.length < 1) {
-          return;
-        }
-
-        var stmt = 'INSERT OR REPLACE INTO oui(oui, name) VALUES($oui, $name)';
-        var parm = {
-          $oui: oui,
-          $name: name
-        };
-
-        db.run(stmt, parm, function(err) {
-          if (err) {
-            console.warn(err);
-            console.warn(stmt);
-            console.warn(parm);
-          }
-        });
-
-        if (last) {
-          ml.rebuilding = false;
-          return next();
-        }
-      })
-
-    };
-
-    fs.unlink(ml.options.sql, function(err) {
-      if (err) return next(err);
-      ml.options.db = new sqlite3.Database(ml.options.sql);
-
-      var stmt = 'CREATE TABLE IF NOT EXISTS oui(oui TEXT PRIMARY KEY, name TEXT)';
-      ml.options.db.run(stmt, function(err) {
-        if (err) {
-          return next(err);
-        }
-        return process(ml.options.db);
-      });
-    });
-
-  }
-
 
   if (this.rebuilding) {
-    return next(new Error('Already Rebuilding'));
+    return next(new Error('maclookup db is already being rebuild'));
   }
+
   this.rebuilding = true;
 
-  try {
-    fs.unlinkSync(this.options.txt);
-  } catch (e) {
-    console.warn('unlinking problem', e);
-  }
-  request(this.options.url)
-    .on('error', console.error)
-    .on('end', function() {
-      parse(next);
-    })
-    .pipe(fs.createWriteStream(this.options.txt));
+  // TODO: rebuild function
+
+  this.rebuilding = false;
 };
 
 MacLookup.prototype.lookup = function(oui, next) {
@@ -125,29 +44,21 @@ MacLookup.prototype.lookup = function(oui, next) {
     return next(new Error('Invalid Request'), null);
   }
 
-  var _oui = oui.split(/[-.:]/).join('').toUpperCase().substring(0, 6);
-  if (_oui.length != 6) {
+  const _oui = oui.split(/[-.:]/).join('').toUpperCase().substring(0, 6);
+  if (_oui.length !== 6) {
     return next(new Error('invalid OUI'));
   }
 
-  var query = 'SELECT * FROM oui WHERE oui=$oui LIMIT 1';
-  var param = {
-    $oui: _oui
-  };
-
-  this.options.db.get(query, param, function(err, row) {
-    next(err ? err : null, (!!row) ? row.name : null);
-  });
-
+  return next ? next(null, this.db[_oui]) : this.db[_oui];
 };
 
 MacLookup.prototype.each = function(each, next) {
-  var query = 'SELECT * FROM oui';
 
-  this.options.db.each(query, function(err, row) {
-    each(err, row);
-  }, next);
+  for (let oui in this.db) {
+    each(null, {oui, name: this.db[oui]})
+  }
 
+  next();
 };
 
 module.exports = new MacLookup();
